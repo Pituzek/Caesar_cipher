2438582f9600b55523b4d24dd92fab0e5cd06768
Spark <piotr.rac@gmail.com>
2023-04-28 00:45:43 +0200

    Add project files.

---
+++ Caesar_cipher/CaesarCipher.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caesar_cipher
+{
+    public static class CaesarCipher
+    {
+        public static Dictionary<char, int> charArray = new Dictionary<char, int>() {
+            { 'A', 0},
+            { 'B', 1},
+            { 'C', 2},
+            { 'D', 3},
+            { 'E', 4},
+            { 'F', 5},
+            { 'G', 6},
+            { 'H', 7},
+            { 'I', 8},
+            { 'J', 9},
+            { 'K', 10},
+            { 'L', 11},
+            { 'M', 12},
+            { 'N', 13},
+            { 'O', 14},
+            { 'P', 15},
+            { 'Q', 16},
+            { 'R', 17},
+            { 'S', 18},
+            { 'T', 19},
+            { 'U', 20},
+            { 'V', 21},
+            { 'W', 22},
+            { 'X', 23},
+            { 'Y', 24},
+            { 'Z', 25}
+        };
+
+        public static string Encode(int key, string text)
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var ch = text[i];
+
+                var encodedChar = cipher(ch, key);
+
+                sb.Append(encodedChar);
+            }
+
+            return sb.ToString();
+        }
+
+        private static char cipher(char ch, int key)
+        {
+            if (!char.IsLetter(ch))
+            {
+                return ch;
+            }
+
+            int index = charArray[ch];
+
+            char encodedChar = charArray.ElementAt((index + key) % 26).Key;
+
+            return encodedChar;
+        }
+    }
+}
---
+++ Caesar_cipher/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Caesar_cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var runTimes = 0;
+
+            while (runTimes < 5)
+            {
+                try
+                {
+                    Console.WriteLine("Podaj klucz.");
+                    int key = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Podaj tekst który chcesz zaszyfrować, dozwolone znaki A-Z");
+                    string text = Console.ReadLine().ToUpper();
+
+                    var result = CaesarCipher.Encode(key, text);
+
+                    Console.WriteLine(result);
+
+                    Console.WriteLine("End? Y/N");
+                    var end = Console.ReadLine().ToUpper().Equals("Y") ? runTimes = 5 : runTimes++;
+
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Klucz moze zawierac tylko i wylacznie wartosci liczbowe");
+                }
+                finally
+                {
+                    runTimes++;
+                }
+            }
+        }
+    }
+}
